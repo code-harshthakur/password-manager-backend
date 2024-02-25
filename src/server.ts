027eb4369041e72d9a255bfe89542c56c836c9c2
Harsh Thakur <codeharsh2002@gmail.com>
2024-02-25 11:40:40 +0530

    Backed Completed

---
+++ src/server.ts
@@ -0,0 +1,65 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { encryptData, saveDataToFile, inputData, decryptData } from "./util";
+import { promises as fs } from "fs";
+import path from "path";
+
+const app = express();
+const PORT: number = 3001;
+
+app.use(bodyParser.json());
+
+app.post("/add", async (req: Request, res: Response) => {
+  const { label, username, password } = req.body;
+
+  const data: inputData = {
+    label,
+    encryptedUsername: encryptData(username),
+    encryptedPassword: encryptData(password),
+  };
+
+  try {
+    await saveDataToFile(data);
+    res.status(200).send("Data Saved Successfully");
+  } catch (error: any) {
+    res.status(500).send(`Error saving the data: ${error.message}`);
+  }
+});
+
+app.post("/search", async (req: Request, res: Response) => {
+  const { label } = req.body;
+
+  if (!label) {
+    res.status(400).send("Label is Required");
+  }
+
+  const filePath = './data/data.json';
+  
+  try {
+    const data = await fs.readFile(filePath,{encoding:'utf-8'});
+    const passwords:inputData[] = JSON.parse(data);
+
+    // Find the entry with the given label
+    const entry = passwords.find(entry => entry.label === label);
+
+    if(!entry) {
+        return res.status(404).send("Label not found");
+    }
+
+    // Decrypt the username and paswword
+    const decryptedUsername = decryptData(entry.encryptedUsername);
+    const decryptedPassword = decryptData(entry.encryptedPassword);
+
+    // Return the decrypted username and password
+    res.json({
+        username:decryptedUsername,
+        password:decryptedPassword
+    });
+  } catch (error) {
+    res.status(500).send(`Error retriving the data: ${error instanceof Error ? error.message : 'Unknown error'}`);
+   }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/util.ts
@@ -0,0 +1,44 @@
+import { promises as fs } from 'fs';
+import CryptoJs from 'crypto-js';
+import dotenv from 'dotenv';
+
+export interface inputData {
+    label:string,
+    encryptedUsername:string,
+    encryptedPassword:string
+}
+
+export const encryptData = (text:string):string => {
+    
+    dotenv.config();
+    const secretKey:string = process.env.SECRET_KEY || '';
+    return CryptoJs.AES.encrypt(text,secretKey).toString();
+}
+
+export const saveDataToFile= async(data:inputData):Promise<void> => {
+    const filePath = './data/data.json';
+    try {
+    
+      let fileData:inputData[];
+      try {
+          const existingData = await fs.readFile(filePath, { encoding: 'utf8' });
+          fileData = JSON.parse(existingData);
+      } catch(error) {
+          fileData = []; // Initialize as an empty object if file reading fails
+      }
+  
+      // updating the data object
+      fileData.push(data);
+      // Write the data into the json file
+      await fs.writeFile(filePath,JSON.stringify(fileData,null,2));
+} catch(error) {
+       throw new Error("Failed to write to file");
+    }
+};
+
+export const decryptData = (cipherText:string):string => {
+    dotenv.config();
+    const secretKey:string = process.env.SECRET_KEY || '';
+    const bytes =  CryptoJs.AES.decrypt(cipherText,secretKey);
+    return bytes.toString(CryptoJs.enc.Utf8);
+}
